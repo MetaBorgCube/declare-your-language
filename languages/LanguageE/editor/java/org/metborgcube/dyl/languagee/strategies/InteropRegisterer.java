65069fd582104b5009631adc613794e9c3d23902
Eelco Visser <e.visser@tudelft.nl>
2015-06-23 22:55:22 +0200

    the magic words: include runprogram_0_0 in interop register and include Java code in the project jar

--- languages/LanguageE/editor/java/org/metborgcube/dyl/languagee/strategies/InteropRegisterer.java
+++ languages/LanguageE/editor/java/org/metborgcube/dyl/languagee/strategies/InteropRegisterer.java
@@ -9,6 +9,6 @@ import org.strategoxt.lang.Strategy;
 public class InteropRegisterer extends JavaInteropRegisterer {
 
   public InteropRegisterer() {
-    super(new Strategy[] { java_strategy_0_0.instance });
+    super(new Strategy[] { java_strategy_0_0.instance, runprogram_0_0.instance  });
   }
 }
