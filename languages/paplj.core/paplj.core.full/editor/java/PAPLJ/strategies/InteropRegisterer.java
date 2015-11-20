bed3b996729015f07fe29d7c3a652600de4ef1f8
Eelco Visser <e.visser@tudelft.nl>
2015-11-20 13:04:37 +0100

    ignore error checking before running interpreter

--- languages/paplj.core/paplj.core.full/editor/java/PAPLJ/strategies/InteropRegisterer.java
+++ languages/paplj.core/paplj.core.full/editor/java/PAPLJ/strategies/InteropRegisterer.java
@@ -3,8 +3,10 @@ package PAPLJ.strategies;
 import org.strategoxt.lang.JavaInteropRegisterer;
 import org.strategoxt.lang.Strategy;
 
+import PAPLJ.strategies.runprogram_0_0;
+
 public class InteropRegisterer extends JavaInteropRegisterer {
     public InteropRegisterer() {
-        super(new Strategy[] { java_strategy_0_0.instance });
+        super(new Strategy[] { java_strategy_0_0.instance, runprogram_0_0.instance  });
     }
 }
