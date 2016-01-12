7c781e9598ab87c9ae64ca6863830ab06368aa05
Vlad Vergu <v.vergu@gmail.com>
2016-01-12 10:01:53 +0100

    Bring back (from the dead apparently) the native strategy to invoke the interpreter

---
+++ paplj/paplj.full/editor/java/paplj/full/strategies/runprogram_0_0.java
@@ -0,0 +1,18 @@
+package paplj.full.strategies;
+
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.strategoxt.lang.Context;
+import org.strategoxt.lang.Strategy;
+
+import ds.generated.interpreter.Generic_A_Program;
+
+public class runprogram_0_0 extends Strategy {
+
+	public static runprogram_0_0 instance = new runprogram_0_0();
+
+	@Override
+	public IStrategoTerm invoke(Context context, IStrategoTerm program) {
+		return new Generic_A_Program(null, program).exec_init().toStrategoTerm(
+				context.getFactory());
+	}
+}
