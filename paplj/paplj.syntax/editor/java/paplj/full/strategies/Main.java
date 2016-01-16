1a959ed1159272990a9a4cc5a6385c6dd54e987b
Eelco Visser <e.visser@tudelft.nl>
2016-01-16 06:43:19 -0500

    project to exercise writing syntax; removed name and type rules, and a few grammar productions

---
+++ paplj/paplj.syntax/editor/java/paplj/full/strategies/Main.java
@@ -0,0 +1,9 @@
+package paplj.full.strategies;
+
+import org.strategoxt.lang.Context;
+       
+public class Main {    
+    public static void init(Context context) {
+        
+    }
+}
---
+++ paplj/paplj.syntax/trans/check/types.ts
@@ -0,0 +1,5 @@
+module types
+
+imports src-gen/signatures/Programs-sig
+src-gen/signatures/Expressions-sig
+src-gen/signatures/Classes-sig
